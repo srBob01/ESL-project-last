17a860cfa1350a9714d77d13be052082b232fab6
srBob01 <necit717@gmail.com>
2023-10-26 17:30:40 +0300

    fixes issue #1

---
+++ blinky/pca10059/mbr/armgcc/_build/nrf52840_xxaa/nrf_memobj.c.d
@@ -0,0 +1,185 @@
+_build/nrf52840_xxaa/nrf_memobj.c.o: \
+ ../../../../../devel/esl-nsdk/components/libraries/memobj/nrf_memobj.c \
+ ../../../../../devel/esl-nsdk/components/libraries/memobj/nrf_memobj.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/stdlib.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_errors.h \
+ ../../../../../devel/esl-nsdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../../../../../devel/esl-nsdk/components/libraries/balloc/nrf_balloc.h \
+ ../config/sdk_config.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/app_util_platform.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52840.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/nrf_assert.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/app_error.h \
+ /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/types.h /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/sys/_pthreadtypes.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_errors.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/nordic_common.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/app_error_weak.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/app_util.h \
+ ../../../../../devel/esl-nsdk/components/softdevice/mbr/headers/nrf_mbr.h \
+ ../../../../../devel/esl-nsdk/components/softdevice/mbr/headers/nrf_svc.h \
+ ../../../../../devel/esl-nsdk/components/libraries/log/nrf_log_instance.h \
+ ../../../../../devel/esl-nsdk/components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/nordic_common.h \
+ ../../../../../devel/esl-nsdk/components/libraries/log/nrf_log_types.h \
+ ../../../../../devel/esl-nsdk/components/libraries/atomic/nrf_atomic.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_common.h \
+ /usr/include/newlib/string.h /usr/include/newlib/sys/string.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_os.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/app_util.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_macros.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/nrf_assert.h
+
+../../../../../devel/esl-nsdk/components/libraries/memobj/nrf_memobj.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/usr/include/newlib/stdlib.h:
+
+/usr/include/newlib/machine/ieeefp.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/newlib.h:
+
+/usr/include/newlib/_newlib_version.h:
+
+/usr/include/newlib/sys/config.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/usr/include/newlib/sys/reent.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/sys/_types.h:
+
+/usr/include/newlib/machine/_types.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/include/newlib/sys/lock.h:
+
+/usr/include/newlib/sys/cdefs.h:
+
+/usr/include/newlib/machine/stdlib.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_errors.h:
+
+../../../../../devel/esl-nsdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+../../../../../devel/esl-nsdk/components/libraries/balloc/nrf_balloc.h:
+
+../config/sdk_config.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/app_util_platform.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52840.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_version.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_compiler.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/mpu_armv7.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/system_nrf52840.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52840_bitfields.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/nrf_assert.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/app_error.h:
+
+/usr/include/newlib/stdio.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h:
+
+/usr/include/newlib/sys/types.h:
+
+/usr/include/newlib/sys/_stdint.h:
+
+/usr/include/newlib/sys/_pthreadtypes.h:
+
+/usr/include/newlib/machine/types.h:
+
+/usr/include/newlib/sys/stdio.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_errors.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/nordic_common.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/app_error_weak.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/app_util.h:
+
+../../../../../devel/esl-nsdk/components/softdevice/mbr/headers/nrf_mbr.h:
+
+../../../../../devel/esl-nsdk/components/softdevice/mbr/headers/nrf_svc.h:
+
+../../../../../devel/esl-nsdk/components/libraries/log/nrf_log_instance.h:
+
+../../../../../devel/esl-nsdk/components/libraries/experimental_section_vars/nrf_section.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/nordic_common.h:
+
+../../../../../devel/esl-nsdk/components/libraries/log/nrf_log_types.h:
+
+../../../../../devel/esl-nsdk/components/libraries/atomic/nrf_atomic.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_common.h:
+
+/usr/include/newlib/string.h:
+
+/usr/include/newlib/sys/string.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_os.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/app_util.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_macros.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/nrf_assert.h:
---
+++ blinky/pca10059/mbr/armgcc/_build/nrf52840_xxaa/nrf_strerror.c.d
@@ -0,0 +1,127 @@
+_build/nrf52840_xxaa/nrf_strerror.c.o: \
+ ../../../../../devel/esl-nsdk/components/libraries/strerror/nrf_strerror.c \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_common.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/string.h ../config/sdk_config.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/nordic_common.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_os.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_errors.h \
+ ../../../../../devel/esl-nsdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/app_util.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52840.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../devel/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../devel/esl-nsdk/components/softdevice/mbr/headers/nrf_mbr.h \
+ ../../../../../devel/esl-nsdk/components/softdevice/mbr/headers/nrf_svc.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_macros.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/nrf_assert.h \
+ ../../../../../devel/esl-nsdk/components/libraries/strerror/nrf_strerror.h \
+ ../../../../../devel/esl-nsdk/components/libraries/util/sdk_errors.h
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_common.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+/usr/include/newlib/string.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/newlib.h:
+
+/usr/include/newlib/_newlib_version.h:
+
+/usr/include/newlib/sys/config.h:
+
+/usr/include/newlib/machine/ieeefp.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/sys/reent.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/usr/include/newlib/sys/_types.h:
+
+/usr/include/newlib/machine/_types.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/include/newlib/sys/lock.h:
+
+/usr/include/newlib/sys/cdefs.h:
+
+/usr/include/newlib/sys/string.h:
+
+../config/sdk_config.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/nordic_common.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_os.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_errors.h:
+
+../../../../../devel/esl-nsdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/app_util.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52840.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_version.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_compiler.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../devel/esl-nsdk/components/toolchain/cmsis/include/mpu_armv7.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/system_nrf52840.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52840_bitfields.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+
+../../../../../devel/esl-nsdk/modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../devel/esl-nsdk/components/softdevice/mbr/headers/nrf_mbr.h:
+
+../../../../../devel/esl-nsdk/components/softdevice/mbr/headers/nrf_svc.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_macros.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/nrf_assert.h:
+
+../../../../../devel/esl-nsdk/components/libraries/strerror/nrf_strerror.h:
+
+../../../../../devel/esl-nsdk/components/libraries/util/sdk_errors.h:
