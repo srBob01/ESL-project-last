be0e518675b4e6c73fe678dc775135d5debd5d34
srBob01 <126959812+srBob01@users.noreply.github.com>
2023-11-01 17:55:49 +0300

    Update main.c

--- work_2/blinky/main.c
+++ work_2/blinky/main.c
@@ -1,57 +1,3 @@
-/**
- *@}
- **/
-
-/**
- * Copyright (c) 2014 - 2021, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- *
- * @defgroup blinky_example_main main.c
- * @{
- * @ingroup blinky_example
- * @brief Blinky Example Application main file.
- *
- * This file contains the source code for a sample application to blink LEDs.
- *
- */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf_delay.h"
@@ -83,23 +29,39 @@ void set_sw(){
     nrf_gpio_cfg_input(MY_SW_1, NRF_GPIO_PIN_PULLUP);
 }
 
+void turn_on_led(int i){
+    nrf_gpio_pin_write(leds[i], 0);
+}
+
+void turn_off_led(int i){
+    nrf_gpio_pin_write(leds[i], 1);
+}
+
+bool is_press(){
+    return !nrf_gpio_pin_read(MY_SW_1);
+}
+
+bool is_not_press(){
+    return !is_press();
+}
+
 void task_1(){
     for(int i = 0; i < LEDS_NUMBER; ++i){
         for(int j = 0; j < number_blinks[i]; ++j){
-            if(nrf_gpio_pin_read(MY_SW_1)){
+            if(is_not_press()){
                 --j;
             }
             else{
-                nrf_gpio_pin_write(leds[i], 0);
+                turn_on_led(i);
                 for(uint32_t k = 0; k < WAIT_TIME; k+=10){
-                    if(nrf_gpio_pin_read(MY_SW_1)){
-                        nrf_gpio_pin_write(leds[i], 1);
+                    if(is_not_press()){
+                        turn_off_led(i);
                         --j;
                         break;
                     }
                     nrf_delay_ms(10);
                 }
-                nrf_gpio_pin_write(leds[i], 1);
+                turn_off_led(i);
                 nrf_delay_ms(WAIT_TIME);
             }
         }
@@ -109,20 +71,20 @@ void task_1(){
 void task_2(){
     for(int i = 0; i < LEDS_NUMBER; ++i){
         for(int j = 0; j < number_blinks[i]; ++j){
-            if(nrf_gpio_pin_read(MY_SW_1)){
+            if(is_not_press()){
                 --j;
             }
             else{
-                nrf_gpio_pin_write(leds[i], 0);
+                turn_on_led(i);
                 for(uint32_t k = 0; k < WAIT_TIME; k+=10){
-                    if(nrf_gpio_pin_read(MY_SW_1)){
+                    if(is_not_press()){
                         nrf_delay_ms(WAIT_TIME - k);
-                        nrf_gpio_pin_write(leds[i], 1);
+                        turn_off_led(i);
                         break;
                     }
                     nrf_delay_ms(10);
                 }
-                nrf_gpio_pin_write(leds[i], 1);
+                turn_off_led(i);
                 nrf_delay_ms(WAIT_TIME);
             }
         }
